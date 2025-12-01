05007b6b62b6f2ce776af5ebf3e90e2da4e43efe
abraham-mendez-code <abraham.me001@gmail.com>
2025-12-01 09:01:32 -0500

    feat(app) Add method to handle result printing
    This commit introduces a generic method for printing query results.
    The method is designed to work with any database, providing a reusable and consistent way to display results across different data sources.
    
    Why:
    - Improves code reusability and maintainability.
    - Ensures consistent output formatting for all database integrations.

--- src/main/java/com/pluralsight/NorthwindApp.java
+++ src/main/java/com/pluralsight/NorthwindApp.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class NorthwindApp {
 
@@ -8,33 +9,99 @@ public class NorthwindApp {
         String username = System.getenv("username");
         String password = System.getenv("password");
 
-        // load the MySQL Driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        // 1. open a connection to the database
-        // use the database URL to point to the correct database
-        Connection connection;
-        connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/northwind",
-                username,
-                password);
+        Scanner scanner = new Scanner(System.in);
 
-        // create statement
-        // the statement is tied to the open connection
-        Statement statement = connection.createStatement();
-        // define your query
-        String query = "SELECT * FROM products ";
+        startMenu(username, password, scanner);
 
-        // 2. Execute your query
-        ResultSet results = statement.executeQuery(query);
+    }
 
-        // process the results
-        while (results.next()) {
-            String city = results.getString("ProductName");
-            System.out.println(city);
+    private static void startMenu(String username, String password, Scanner scanner) {
+        // try to connect to the northwind database using the username and password we provided
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password)){
+
+            while (true){
+                System.out.print("""
+                        What do you want to do?
+                            1) Display All Products
+                            2) Display All Customers
+                            0) Exit
+                        Select an option:\s""");
+
+                switch (scanner.nextInt()) {
+                    case 0:
+                        System.out.println("Goodbye!");
+                        System.exit(0);
+                    case 1:
+                        displayAllProducts(connection);
+                        break;
+                    case 2:
+                        //displayAllCustomers(connection);
+                        break;
+                    default:
+                        System.out.println("Invalid choice");
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Could not connect to DB");
+            System.exit(1);
+        }
+    }
+
+
+    private static void displayAllProducts(Connection connection) {
+
+        try (
+
+                PreparedStatement preparedStatement = connection.prepareStatement("""
+                        SELECT
+                            ProductID,
+                            ProductName,
+                            UnitPrice,
+                            UnitsInStock
+                        FROM
+                            Products
+                        ORDER BY
+                            ProductName;
+                        """
+                );
+
+                ResultSet results = preparedStatement.executeQuery();
+
+        ) {
+            printResults(results);
+        } catch (SQLException e) {
+            System.out.println("Could not get all the products");
+            System.exit(1);
         }
 
-        // 3. Close the connection
-        connection.close();
+    }
+
+
+    //this method will be used in the displayMethods to actually print the results to the screen
+    private static void printResults(ResultSet results) throws SQLException {
+        //get the metadata so we have access to the field names
+        ResultSetMetaData metaData = results.getMetaData();
+        //get the number of rows returned
+        int columnCount = metaData.getColumnCount();
+
+        while (results.next()) {
+
+            for (int i = 1; i <= columnCount; i++) {
+                // gets the current column name
+                String columnName = metaData.getColumnName(i);
+
+                // get the current column value
+                String value = results.getString(i);
+
+                // print out the column name and column value
+                System.out.println(columnName + ": " + value + " ");
+            }
+
+            // prints an empty line to make the results prettier
+            System.out.println();
+
+        }
 
     }
 
